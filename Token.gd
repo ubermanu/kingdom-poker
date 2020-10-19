5b4ef9436e1562251e57462a495f332d58db8bce
ubermanu <e.vodor@gmail.com>
2020-10-20 00:03:54 +0200

    Remove unnecessary code

--- Token.gd
+++ Token.gd
@@ -3,29 +3,18 @@ extends KinematicBody2D
 var dragging = false
 var offset = Vector2.ZERO
 
-signal dragsignal
-
-func _ready():
-	connect("dragsignal", self, "_set_drag_pc")
-
-func _process(delta):
+func _process(_delta):
 	if dragging:
-		var mousepos = get_viewport().get_mouse_position()
-		self.position = Vector2(mousepos.x - offset.x, mousepos.y - offset.y)
+		var mouse = get_viewport().get_mouse_position()
+		position = Vector2(mouse.x - offset.x, mouse.y - offset.y)
 
-func _set_drag_pc():
-	dragging = !dragging
-	offset = get_viewport().get_mouse_position() - position
-
-func _on_Token_input_event(viewport, event, shape_idx):
+func _on_Token_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
-			emit_signal("dragsignal")
+			offset = get_viewport().get_mouse_position() - position
+			dragging = true
 		elif event.button_index == BUTTON_LEFT and !event.pressed:
-			emit_signal("dragsignal")
-	elif event is InputEventScreenTouch:
-		if event.pressed and event.get_index() == 0:
-			self.position = event.get_position()
+			dragging = false
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
