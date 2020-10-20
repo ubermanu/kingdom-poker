5d5a76d6ab5eaea1c696da1220e8e7dff7938b42
ubermanu <e.vodor@gmail.com>
2020-10-20 09:03:59 +0200

    Add background sounds

--- MusicButton.gd
+++ MusicButton.gd
@@ -3,17 +3,21 @@ extends Area2D
 export (bool) var active = true
 
 func _ready():
-	update_icon()
+	update()
 
-func update_icon():
+func update():
 	if active:
 		$Icon.frame_coords.x = 0
+		$Music.play()
+		$Crowd.play()
 	else: 
 		$Icon.frame_coords.x = 1
+		$Music.stop()
+		$Crowd.stop()
 
 func toggle():
 	active = !active
-	update_icon()
+	update()
 
 func _on_MusicButton_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
