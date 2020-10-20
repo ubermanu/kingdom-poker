1da8d3a40757e58dd36990446f60325ec6e1369c
ubermanu <e.vodor@gmail.com>
2020-10-20 14:09:07 +0200

    Reveal cards on load wip

--- Game.gd
+++ Game.gd
@@ -9,10 +9,23 @@ func _ready():
 	$TokenList.visible = false
 	cards = range(52)
 	shuffle()
+	update_cards()
+	flip_cards(true)
+
+func update_cards():
 	var ids = cards.slice(0, 4)
-	print(ids)
 	$Board/Card.id = ids[0]
-	$Board/Card.flipped = false
+	$Board/Card2.id = ids[1]
+	$Board/Card3.id = ids[2]
+	$Board/Card4.id = ids[3]
+	$Board/Card5.id = ids[4]
+
+func flip_cards(flipped):
+	$Board/Card.flipped = flipped
+	$Board/Card2.flipped = flipped
+	$Board/Card3.flipped = flipped
+	$Board/Card4.flipped = flipped
+	$Board/Card5.flipped = flipped
 
 func shuffle():
 	randomize()
