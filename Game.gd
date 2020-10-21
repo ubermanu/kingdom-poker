49b7af0964ef7e715576e35f0ae0846777c85776
ubermanu <e.vodor@gmail.com>
2020-10-21 22:42:12 +0200

    Update messages and setup real rounds

--- Game.gd
+++ Game.gd
@@ -16,8 +16,6 @@ func _ready():
 	$TokenList/Token2.connect("bet", self, "bet")
 	$TokenList/Token3.connect("bet", self, "bet")
 	$TokenList/Token4.connect("bet", self, "bet")
-	set_bank(bank)
-	set_pot(pot)
 	runtime()
 
 func set_bank(amount):
@@ -35,6 +33,8 @@ func bet(amount):
 
 func runtime():
 	# Reset setup
+	set_bank(bank)
+	set_pot(pot)
 	$TokenList.visible = false
 	$Board.visible = false
 	flip_cards(true)
@@ -42,6 +42,7 @@ func runtime():
 	update_cards()
 	
 	# Show token list
+	$Message.text = "Veuillez miser..."
 	$TokenList.visible = true
 	$TokenList/Token.draggable = true
 	$TokenList/Token2.draggable = true
@@ -49,15 +50,23 @@ func runtime():
 	$TokenList/Token4.draggable = true
 	yield(self, "step_finished")
 	
+	# Select cards to swap
+	$Message.text = "Changer certaines cartes?"
 	$TokenList.visible = false
 	$Board.visible = true
 	flip_cards(false)
 	yield(self, "step_finished")
 	
+	# Result
+	# TODO: Generate result according to card combos
+	flip_cards(false)
+	$Message.text = "Perdu, on retente sa chance?"
+	
 	if bank <= 0:
 		# TODO: End game
 		pass
 	
+	yield(self, "step_finished")
 	runtime()
 
 func update_cards():
